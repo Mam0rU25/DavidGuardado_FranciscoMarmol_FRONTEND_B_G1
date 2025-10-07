da7cf8aa02a993cfe348b0a2e42d4bcfa80188f1
FranciscoMarmol <20240348@ricaldone.edu.sv>
2025-10-07 09:55:55 -0600

    PRUEBASSS

--- js/Service/PeliculasService.js
+++ js/Service/PeliculasService.js
@@ -1,15 +1,15 @@
-// js/services/productService.js
-const API_URL = "https://learnapifront-9de8a2348f9a.herokuapp.com/api/products";
+// js/services/Movies.js
+const API_URL = "localhost//:8080";
  
 export async function getProducts(page = 0, size = 10) {
-  const res = await fetch(`${API_URL}/getAllProducts`, {
+  const res = await fetch(`${API_URL}/getAllMovies`, {
     credentials: "include",
   });
   return res.json();
 }
  
-export async function createProduct(data) {
-  await fetch(`${API_URL}/newProduct`, {
+export async function createMovie(data) {
+  await fetch(`${API_URL}/newMovie`, {
     credentials: "include",
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -17,8 +17,8 @@ export async function createProduct(data) {
   });
 }
  
-export async function updateProduct(id, data) {
-  await fetch(`${API_URL}/updateProduct/${id}`, {
+export async function updateMovie(id, data) {
+  await fetch(`${API_URL}/updateMovie/${id}`, {
     credentials: "include",
     method: "PUT",
     headers: { "Content-Type": "application/json" },
@@ -26,8 +26,8 @@ export async function updateProduct(id, data) {
   });
 }
  
-export async function deleteProduct(id) {
-  await fetch(`${API_URL}/deleteProduct/${id}`, {
+export async function deleteMovie(id) {
+  await fetch(`${API_URL}/deleteMovie/${id}`, {
     credentials: "include",
     method: "DELETE",
   });
